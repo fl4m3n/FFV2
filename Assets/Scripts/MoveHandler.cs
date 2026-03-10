2cacf57a214902974c925c6dae0432baab0f6d5a
Jelmer-W <104435566+Jelmer-W@users.noreply.github.com>
2026-03-10 18:16:15 +0100

    WebSocketReceiver in Unity setup

---
+++ Assets/Scripts/MoveHandler.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class MoveHandler : MonoBehaviour
+{
+
+    void Update()
+    {
+    }
+
+    public void ReceiveMovementMessage(string axis, string value)
+    {
+    }
+}
