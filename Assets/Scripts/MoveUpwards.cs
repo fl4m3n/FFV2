3691b05382cd4d4bd1ba171e31eae9786d601e0b
fl4m3n <107616589+fl4m3n@users.noreply.github.com>
2026-03-15 17:51:34 +0100

    new environment
    trying out a new environment

--- Assets/Scripts/MoveUpwards.cs
+++ Assets/Scripts/MoveUpwards.cs
@@ -4,9 +4,29 @@ public class MoveUpwards : MonoBehaviour
 {
     [Tooltip("Speed in m/s")]
     public float speed = 20f;
+    public float startHeight = 12000f;
+    public bool isMoving = false;
 
     void Update()
     {
-        transform.Translate(Vector3.up * speed * Time.deltaTime, Space.World);
+        if (isMoving)
+        {
+            transform.Translate(Vector3.up * speed * Time.deltaTime, Space.World);
+        }
+    }
+    
+    public void SetMoving(bool status)
+    {
+        isMoving = status;
+        if (!isMoving)
+        {
+            ResetToStart();
+        }
+    }
+    private void ResetToStart()
+    {
+        Vector3 currentPos = transform.position;
+        currentPos.y = startHeight;
+        transform.position = currentPos;
     }
 }
